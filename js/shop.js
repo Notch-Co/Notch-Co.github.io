1fc8ca177a3d0dcdb186efde25a81480d88d162a
Xuan Pham <hpham024@uottawa.ca>
2023-11-19 16:13:03 -0500

    js for adding duplicate items to cart

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
-function addProduct(elementID){
-    let titleElement = document.getElementById("cart-header");
+function addProduct(elementID, flag){
+    const titleElement = document.getElementById("cart-header");
     let title = titleElement.innerHTML;
     
     let firstBrac = title.indexOf("(");
@@ -14,25 +14,42 @@ function addProduct(elementID){
 
     titleElement.innerHTML = title.slice(0, firstBrac + 1) + num + title.slice(secBrac)
 
-    let options = "\t<option selected>1</option>";
-    for (let i = 2; i <= 50; i++){
-        options += ("\t<option>" + i + "</option>\n");
-    } 
     const imgPath = document.getElementById(elementID + "-img").src;
     const productName = document.getElementById(elementID + "-prod-name").innerHTML;
     const price = document.getElementById(elementID + "-price").innerHTML;
 
-    cart = document.getElementById("cart");
+    const cart = document.getElementById("cart");
 
-    cart.innerHTML = cart.innerHTML + "\n" 
-    + "<div class=\"cart-item\">"
-    + "\t<img class=\"prod-img\" src=\"" + imgPath + "\" alt=\"" + productName + " image\">\n"
-    + "\t<h6 class=\"prod-name\">" + productName + "</h6>\n"
-    + "\t<select>\n" + options + "</select>\n"
-    + "\t<p class=\"price\">" + price + "</p>\n"
-    + "</div>";
+    if(!flag){  // the product hasn't been added yet so generate a new one
+        let options = "\t<option selected>1</option>";
+        for (let i = 2; i <= 50; i++){
+            options += ("\t<option>" + i + "</option>\n");
+        } 
+
+        cart.innerHTML = cart.innerHTML + "\n" 
+        + "<div class=\"cart-item\">"
+        + "\t<img class=\"prod-img\" src=\"" + imgPath + "\" alt=\"" + productName + " image\">\n"
+        + "\t<h6 class=\"prod-name\">" + productName + "</h6>\n"
+        + "\t<select id=\"cart-" + elementID + "-num\">\n" + options + "</select>\n"
+        + "\t<p class=\"price\" id=\"cart-" + elementID + "-price\">" + price + "</p>\n"
+        + "</div>";
+
+    } else {
+        const selectElem = document.getElementById("cart-" + elementID + "-num")
+        const currentSel = selectElem.selectedIndex
+        let num = parseInt(selectElem.options[currentSel].text) + 1;
+        selectElem.options[currentSel + 1].selected = true;
+
+        let totalPrice = parseFloat(price.slice(price.indexOf("$") + 1)) * num;
+        const priceElem = document.getElementById("cart-" + elementID + "-price")
+        priceElem.innerHTML = "$" + totalPrice + ".00";
+    }
 }
 
-document.getElementById("buy-pen").addEventListener("click", function(){addProduct("pen")});
-document.getElementById("buy-stencil").addEventListener("click", function(){addProduct("stencil")});
-document.getElementById("buy-strap").addEventListener("click", function(){addProduct("strap")});
+let flagPen = false;
+let flagStencil = false;
+let flagStrap = false;
+
+document.getElementById("buy-pen").addEventListener("click", function(){addProduct("pen", flagPen); flagPen = true;});
+document.getElementById("buy-stencil").addEventListener("click", function(){addProduct("stencil", flagStencil); flagStencil = true;});
+document.getElementById("buy-strap").addEventListener("click", function(){addProduct("strap", flagStrap); flagStrap = true;});
