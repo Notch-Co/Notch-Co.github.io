bf7b327faa075ca3820760d3695c602c75e315de
Batool Agiel <bagie008@uottawa.ca>
2023-11-21 16:11:46 -0500

    more FAQ code

--- js/faq.js
+++ js/faq.js
@@ -8,23 +8,47 @@ function hide_item(elementID){
     acc.className = "panel";
 }
 
-let display = true;
+let refund_display = true;
+let ship_display = true;
+let feat_display = true;
+let functionality_display = true;
 
 document.getElementById("refund-button").addEventListener("click", function(){
-    if (display){
+    if (refund_display){
         display_item("refund");
-        display = false;
+        refund_display = false;
     } else {
         hide_item("refund");
-        display = true;
+        refund_display = true;
     }
+});
 
-    document.getElementById("shipping-button").addEventListener("click", function(){
-        if (display){
-            display_item("shipping");
-            display = false;
+document.getElementById("shipping-button").addEventListener("click", function(){
+    if (ship_display){
+        display_item("shipping");
+        ship_display = false;
+    } else {
+        hide_item("shipping");
+        ship_display = true;
+    }
+});
+
+document.getElementById("features-button").addEventListener("click", function(){
+        if (feat_display){
+            display_item("features");
+            feat_display = false;
         } else {
-            hide_item("shipping");
-            display = true;
-        }
+            hide_item("features");
+            feat_display = true;
+    }
+});
+
+document.getElementById("functionality-button").addEventListener("click", function(){
+        if (functionality_display){
+            display_item("functionality");
+            functionality_display = false;
+        } else {
+            hide_item("functionality");
+            functionality_display = true;
+    }
 });
